2fec86e4e6602701ef6c40d4d8130415d5bbdc3f
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-04-13 20:42:24 +0800

    Removed secret key (CSRF) from __init__.py
    Lecturer said not to share our secret key on the repo. If I'm not wrong, he said he would elaborate in another lecture. It would be better to store the secret key in a separate file called config.py. We can discuss this in the next meeting.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
+from config import Config
 from dotenv import load_dotenv
 load_dotenv('.flaskenv')  # Load environment variables from .flaskenv
 import os
 
-# This will automatically read from the .flaskenv / .env if using Flask CLI
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config.from_object(Config) 
 
 from app import routes
