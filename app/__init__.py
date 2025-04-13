3696a79370de77d897e1a06584684bcbd0e057c7
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-04-13 23:41:32 +0800

    Added db, db migrations and login
    Included packages necessary for database and login features.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from config import Config
 from dotenv import load_dotenv
+from flask_login import LoginManager
 load_dotenv('.flaskenv')  # Load environment variables from .flaskenv
 import os
 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
+
 app = Flask(__name__)
 app.config.from_object(Config) 
+db = SQLAlchemy(app) 
+migrate = Migrate(app, db) 
+login = LoginManager(app) 
+login.login_view = 'login'
 
-from app import routes
+from app import routes, models
