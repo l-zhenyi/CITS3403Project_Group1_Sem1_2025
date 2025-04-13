bc1094f639931e0afd2602e51e92261a03f8631d
Jacob Read <23970936@student.uwa.edu.au>
2025-04-13 16:11:51 +0800

    Improved security by hiding secret key in .flaskenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv('.flaskenv')  # Load environment variables from .flaskenv
+import os
+
+# This will automatically read from the .flaskenv / .env if using Flask CLI
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lol' # Replace with a secure key in production
+app.config['SECRET_KEY'] = SECRET_KEY
 
 from app import routes
