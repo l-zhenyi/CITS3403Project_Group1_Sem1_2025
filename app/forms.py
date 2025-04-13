a08640288790cbb22f1b032a0b1b358c612213db
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-04-13 23:05:40 +0800

    Added username and email validation
    Also reviewed the existing code. It is a good idea to add a length requirement.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -26,3 +27,13 @@ class RegistrationForm(FlaskForm):
         validators=[DataRequired(), EqualTo('password', message='Passwords must match.')]
     )
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username): 
+        user = User.query.filter_by(username=username.data).first() 
+        if user is not None: 
+            raise ValidationError('Please use a different username.') 
+ 
+    def validate_email(self, email): 
+        user = User.query.filter_by(email=email.data).first() 
+        if user is not None: 
+            raise ValidationError('Please use a different email address.') 
