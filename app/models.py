90a77bd3389dc50eb2d611de3c557fc8dfefceb4
Jacob Read <23970936@student.uwa.edu.au>
2025-04-21 21:25:47 +0800

    Updated the user model to SQLAlchemy2.0 syntax

--- app/models.py
+++ app/models.py
@@ -1,23 +1,27 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+from typing import Annotated, Optional
 
 class User(UserMixin, db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    username = db.Column(db.String(64), index=True, unique=True) 
-    email = db.Column(db.String(120), index=True, unique=True) 
-    password_hash = db.Column(db.String(128)) 
+    __tablename__ = "user"
 
-    def __repr__(self): 
-        return '<User {}>'.format(self.username)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    email: Mapped[str] = mapped_column(String(120), index=True, unique=True)
+    password_hash: Mapped[str] = mapped_column(String(128))
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
     
-    def set_password(self, password): 
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256') 
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
 
-    def check_password(self, password): 
-        return check_password_hash(self.password_hash, password) 
-    
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
-@login.user_loader 
-def load_user(id): 
-    return User.query.get(int(id)) 
+@login.user_loader
+def load_user(id: str) -> Optional[User]:
+    return db.session.get(User, int(id))
